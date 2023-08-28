7fedf11297c2f4771eb8ae6b1302097136a48e92
skyum1 <rjftkd126@gmail.com>
2023-08-28 22:12:42 +0900

    study 3 state,props

---
+++ src/components/TOC.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render() {
+      var lists = [];
+      var data = this.props.data;
+      var i = 0;
+      while(i < data.length) {
+        // 다중으로 dom요소 추가시 key값을 넣어줘야 warning 제거
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        i++;
+
+      }
+
+      return (
+        <nav>
+              <ul>
+                  {lists}
+              </ul>
+          </nav>
+      );
+    }
+  }
+
+  export default TOC;
