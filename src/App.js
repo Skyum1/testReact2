37dbaeb170760d8cb54804a22bf95923d291a349
skyum1 <rjftkd126@gmail.com>
2023-08-29 21:55:42 +0900

    state, event, bind

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) { //생성자
     super(props);
     this.state = {
+      mode:'read',
       subject: {title:'WEB', sub:'Wolrd wide Web!!'},
+      welcome: {title:'Welcome', desc:'Hello, React!!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is HyperText....'},
         {id:2, title:'CSS', desc:'CSS is design....'},
@@ -17,16 +19,39 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    console.log('render',this);
     return (
     <div className="App">
-      <Subject 
+      {/* <Subject 
         title={this.state.subject.title} 
         sub={this.state.subject.sub}>
-      </Subject>    
+      </Subject>     */}
+      <header>
+              <h1>
+                  <a href="/" onClick={function(e){
+                    console.log('event in', this);
+                    e.preventDefault();
+                    return;
+                    this.state.mode = 'welcome';
+                    this.setState({
+                      mode:'welcome'
+                    });
+                  }.bind(this)}>{this.state.subject.title}</a>
+              </h1>
+              {this.state.subject.sub}
+          </header>
       <TOC
         data={this.state.contents}>
       </TOC>   
-      <Content title="HTML" desc=" HTML is HyperText Markup Language.!!!!"/>
+      <Content title={_title} desc={_desc}/>
     </div>
     );
   }
