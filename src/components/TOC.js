76c27501ad733f1904c1b439c8010361fa3a9b12
skyum1 <rjftkd126@gmail.com>
2023-09-11 21:13:19 +0900

    230911

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -7,7 +7,18 @@ class TOC extends Component {
       var i = 0;
       while(i < data.length) {
         // 다중으로 dom요소 추가시 key값을 넣어줘야 warning 제거
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        lists.push(
+          <li key={data[i].id}>
+            <a 
+              href={"/content/"+data[i].id}
+              data-id={data[i].id}
+              onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage(e.target.dataset.id);
+              }.bind(this)}
+            >{data[i].title}
+            </a>
+          </li>);
         i++;
 
       }
